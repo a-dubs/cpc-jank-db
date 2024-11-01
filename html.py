d57fb4972708de79da912ef1a3535a93ee402085
uhryniuk <dylan.uhryniuk@canonical.com>
2024-11-01 03:09:29 -0500

    feat: frontend

---
+++ html.py
@@ -0,0 +1,68 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime as dt
+
+SUCCESS = "✅"
+WARNING = "⚠️"
+FAILURE = "❌"
+
+# Set up Jinja environment
+template_dir = 'templates'  # Directory where your templates are stored
+env = Environment(
+    loader=FileSystemLoader(template_dir),
+    autoescape=select_autoescape(['html', 'xml', 'css'])  # Enable autoescaping for HTML/XML templates
+)
+
+# Load a specific template
+template = env.get_template('other.html')  # Replace with your template file name
+
+t = {
+    "test_name": "tests.BasicUbuntu.test_snap_preseed_optimized",
+    "cases": [{"link": "blorp", "name": "hello moto"}]
+}
+
+j = {
+    "suite": "Jammy",
+    "family": "Base",
+    "build_state": SUCCESS,
+    "upload_state": SUCCESS,
+    "test_state": WARNING,
+    "pass_count": 5,
+    "warn_count": 4,
+    "fail_count": 0,
+    "tests": [t,t,t]
+}
+
+oracle = {
+    "name": "Oracle",
+    "jobs": [j,j,j]
+}
+
+ibm = {
+    "name": "IBM",
+    "jobs": [j,j]
+}
+
+
+# Define template context data
+context = {
+    'timestamp': dt.datetime.now(),
+    'projects': [ibm, oracle]
+}
+
+# Render template with context
+rendered_output = template.render(context)
+
+# Print or use the rendered template output
+print(rendered_output)
+
+# Render a template with provided context and write it to a file
+def render_template_to_file(template_name, context, output_file):
+    template = env.get_template(template_name)
+    output = template.render(context)
+    
+    # Write the rendered template to a file
+    with open(output_file, 'w') as file:
+        file.write(output)
+    print(f"Template rendered and saved to {output_file}")
+
+
